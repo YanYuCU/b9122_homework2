3f25daffc6eca8869a047036252730cd6bceb61e
YanYuCU <yy3184@columbia.edu>
2022-10-09 23:16:37 -0400

    Update data structure of seen
    Use a set rather than a list to store the URLs already seen, so we can increase the speed when we check whether a given URL is in seen URLs

--- webcrawler.py
+++ webcrawler.py
@@ -5,7 +5,7 @@ import urllib.request
 seed_url = "https://www8.gsb.columbia.edu"
 
 urls = [seed_url]    #queue of urls to crawl
-seen = [seed_url]    #stack of urls seen so far
+seen = set([seed_url])    #stack of urls seen so far
 opened = []          #we keep track of seen urls so that we don't revisit them
 
 maxNumUrl = 10; #set the maximum number of urls to visit
